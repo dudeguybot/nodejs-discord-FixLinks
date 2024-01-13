b85d3477d32b46c493bd4224d3ae3e39ac3d9786
dudeguybot <156486250+dudeguybot@users.noreply.github.com>
2024-01-12 20:39:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js'); //import client from discord
 const keep_alive = require('./keep_alive.js');
 const client = new Discord.Client();
+String fixedText = "";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,11 +12,24 @@ client.on('message', msg => {
     if (msg.author == client.user) {
       return;
     }
-    else if (msg.content === 'ping') {
-      msg.reply('Pong!');
+    else if (msg.content.includes("twitter.com") ) {
+      fixedstring = x.replace("twitter.com","fxtwitter.com")
+      msg.reply(fixedstring);
     }
+    else if (msg.content.includes("x.com") ) {
+      fixedstring = x.replace("x.com","fxtwitter.com")
+      msg.reply(fixedstring);
+    }
+    else if (msg.content.includes("tiktok.com") ) {
+      fixedstring = x.replace("tiktok.com","vxtiktok.com")
+      msg.reply(fixedstring);
+    }
+    else if (msg.content.includes("reddit.com") ) {
+      fixedstring = x.replace("reddit.com","vxreddit.com")
+      msg.reply(fixedstring);
+    }       
     else {
-        msg.reply(msg.content);
+        //msg.reply(msg.content);
     }
 });
 
